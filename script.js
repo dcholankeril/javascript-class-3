8518ae20baa76c3f0715f0152137d89cd32178e3
Diana Cholankeril <dianac@id.iit.edu>
2017-04-14 11:35:53 -0500

    for the variable t = 0, while t < 4, it calls the function aSingleDivYo from the function that was created before; each time 1 will be added to t

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-
-for (var t = 0; t < 4; t++) {
+function addASingleDivYo() {
     var aSingleDivYo = document.createElement("div");
     aSingleDivYo.className ="whatever";
     document.body.appendChild(aSingleDivYo);
 
+}
+
+
+for (var t = 0; t < 4; t++) {
+    addASingleDivYo();
+
+}
+
+var t = 0;
+do {
+    addASingleDivYo();
+    t++;
+} while (t < 4);
+
+var t = 0;
+while ( t < 4) {
+    addASingleDivYo();
+    t++;
 }
