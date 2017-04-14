e69141ba0eefb88711b993e1021f27d7c670996c
Diana Cholankeril <dianac@id.iit.edu>
2017-04-14 11:25:08 -0500

    did some things with asingledivyo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var aSingleDivYo = document.createElement("div");
-aSingleDivYo.className ="whatever";
 
-document.appendChild(aSingleDivYo);
+for (var t = 0; t < 4; t++) {
+    var aSingleDivYo = document.createElement("div");
+    aSingleDivYo.className ="whatever";
+    document.body.appendChild(aSingleDivYo);
+
+}
